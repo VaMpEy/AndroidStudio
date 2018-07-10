f0a58dbf01ce3ff886cdf1b75ac2be75ad79384a
Udo Rinne <ur86@outlook.de>
2018-07-10 16:14:37 +0200

    done

--- AndroidFiAe02/app/src/main/java/urinnenet/de/androidfiae02/MenuActivity.java
+++ AndroidFiAe02/app/src/main/java/urinnenet/de/androidfiae02/MenuActivity.java
@@ -2,9 +2,12 @@ package urinnenet.de.androidfiae02;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MenuActivity extends Activity {
 
@@ -31,5 +34,32 @@ public class MenuActivity extends Activity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.my_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_item_1:
+                actionItem4onClicked(item);
+                break;
+
+            case R.id.action_item_2:
+                Toast.makeText(this,"item 2 (TOASTy)",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_item_3:
+                actionItem4onClicked(item);
+                break;
+        }
+        return true;
+    }
+
+    public void actionItem4onClicked(MenuItem item) {
+        Toast.makeText(this,item.getTitle(),Toast.LENGTH_SHORT).show();
+    }
+
+    public void showOtherActivity(MenuItem item) {
+        Intent intent = new Intent(this,OtherActivity.class);
+        startActivity(intent);
     }
 }
